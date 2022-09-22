d0bfaf17bdf8164e2432dca911913cb5ee55e898
alemedinabjj <101877712+alemedinabjj@users.noreply.github.com>
2022-09-21 22:25:34 -0300

    'ajuste'

--- script.js
+++ script.js
@@ -10,13 +10,17 @@ let namechange = document.getElementById("namechange");
 let day = document.getElementById("dia");
 let month = document.getElementById("mes");
 
+const nameValid = document.querySelector("#nameValid");
+const numValid = document.querySelector("#numValid");
 
 const regexCardByType = {
   Visa: /^4[0-9]{12}(?:[0-9]{3})/,
-  Mastercard: /^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$/,
+  Mastercard:
+    /^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$/,
   Amex: /^3[47][0-9]{13}/,
   DinersClub: /^3(?:0[0-5]|[68][0-9])[0-9]{11}/,
-  Discover: /^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$/,
+  Discover:
+    /^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$/,
   JCB: /^(?:2131|1800|35\d{3})\d{11}$/,
 };
 
@@ -31,11 +35,11 @@ const getCardType = (cardNumber) => {
 const handleCardType = (event) => {
   const { value } = event.target;
   const cardType = getCardType(value);
-  let cardFormatted = cardType.toString().toLocaleLowerCase()
+  let cardFormatted = cardType.toString().toLocaleLowerCase();
   const cardTypeElement = document.getElementById("card-type");
 
-  for(let i in cardType) {
-    if(cardType[i] === cardFormatted) {
+  for (let i in cardType) {
+    if (cardType[i] === cardFormatted) {
       return (cardTypeElement.src = `./assets/${cardFormatted}.svg`);
     }
 
@@ -46,7 +50,6 @@ const handleCardType = (event) => {
     cardTypeElement.src = "./assets/unknown.svg";
   }
 };
-
 
 const maskInputNumCard = (value) => {
   return value
@@ -63,13 +66,26 @@ const maskInputCvv = (value) => {
 const handleFormChange = (event) => {
   const { value, id } = event.target;
 
+  if (id === "numcard" && value.length > 1) {
+    numCard.classList.remove("border-red-500");
+    numValid.style.opacity = 0;
+  }
+
+  if (id === "namecard" && value.length > 1) {
+    nameCard.classList.remove("border-red-500");
+    nameValid.style.opacity = 0;
+  }
+
   if (id === "numcard") {
-    numchange.innerText = maskInputNumCard(value);
-  } else if (id === "namecard") {
-    namechange.innerText = value;
-  } else if (id === "cvv") {
-    ccvvalue.innerHTML = maskInputCvv(value);
-    console.log(cvv);
+    return (numchange.innerText = maskInputNumCard(value));
+  }
+
+  if (id === "cvv") {
+    return (ccvvalue.innerText = maskInputCvv(value));
+  }
+
+  if (id === "namecard") {
+    return (namechange.innerText = value);
   }
 
   if (id === "dia") {
@@ -81,20 +97,32 @@ const handleFormChange = (event) => {
 
 const handleFormSubmit = (event) => {
   event.preventDefault();
+  const { value: numCardValue } = numCard;
+  
 
-  if (
-    numCard.value.length < 16 ||
-    nameCard.value.length < 3 ||
-    cvv.value.length < 3
-  ) {
-    alert("Preencha todos os campos corretamente");
-    numCard.classList.add("border-red-500");
+  if (nameCard.value === "") {
+    nameValid.style.opacity = 1;
     nameCard.classList.add("border-red-500");
-    cvv.classList.add("border-red-500");
-  } else {
-    alert("Pagamento realizado com sucesso");
   }
+
+  if (numCardValue.length < 16) {
+    numValid.style.opacity = 1;
+    numCard.classList.add("border-red-500");
+  }
+
+  if (numCardValue.length < 16 || nameCard.value === "") {
+    return
+  }
+
+  return alert("Formulário enviado com sucesso!");
 };
+
+
+function numberOnly(id) {
+  const element = document.getElementById(id);
+  element.value = element.value.replace(/[^0-9]/gi, "");
+  element.maxLength = 16;
+}
 
 numCard.addEventListener("input", handleFormChange);
 numCard.addEventListener("input", handleCardType);
